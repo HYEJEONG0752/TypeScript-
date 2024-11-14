1b0a7d90671f84edd1c13f805e134ff5d5f28d2d
HYEJEONG0752 <hj07090752@gmail.com>
2024-11-14 23:06:27 +0900

    TypeScript 기본 타입 실습 코드 추가

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+// string
+let myString = "Hello, TypeScript!";
+console.log(myString); // Hello, TypeScript!
+// number
+let myNumber = 42;
+console.log(myNumber); // 42
+// boolean
+let myBoolean = true;
+console.log(myBoolean); // true
+// null 
+let myNull = null;
+console.log(myNull); // null
+// any 
+let myAny = "I can be anything!";
+console.log(myAny);
+myAny = "123";
+console.log(myAny);
+myAny = false;
+console.log(myAny);
+// any 배열
+let anyList = [123, "가나다", true];
+console.log(anyList);
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+// string
+let myString: string = "Hello, TypeScript!";
+console.log(myString);  // Hello, TypeScript!
+
+// number
+let myNumber: number = 42;
+console.log(myNumber);  // 42
+
+// boolean
+let myBoolean: boolean = true;
+console.log(myBoolean); // true
+
+// null 
+let myNull: null = null;
+console.log(myNull);    // null
+
+// any 
+let myAny: any = "I can be anything!";
+console.log(myAny); // I can be anything!
+
+myAny = "123";
+console.log(myAny); // 123
+
+myAny = false;
+console.log(myAny); // false
+
+// any 배열
+let anyList: any[] = [123,"가나다",true];
+
+console.log(anyList);   // [ 123, '가나다', true ]
+
+
